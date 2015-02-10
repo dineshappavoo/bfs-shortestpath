0572a570c4d6a7a2775e7f2658839b5bb10fb52b
dinesgappavoo <dinesha.cit@gmail.com>
2015-02-10 05:46:35 -0600

    Added readme file

---
+++ README.md
@@ -0,0 +1,34 @@
+BFS - Shortest Path 
+=======================
+
+BFS shortest for undirected graph
+
+##Complexity
+	  Worst case performance  O (|E|)
+	  Worst case space complexity O (|V|)
+
+##Install
+
+This library has the implementaion of BFS to find the shortest path in a undirected graph 
+G=[V,E].The following code snippet shows how to get the shortest path,
+
+    	BFSShortestPath.findShortestPath();
+
+
+###Input
+	3 3
+	1 2 
+	2 3 
+	1 3 
+	
+	1 3
+
+First integer is the total number of vertices |V| in the graph G. The next integer is the number of edges |E| in the graph.
+Next |E| lines has the edges information (u, v). All inputs must be given through terminal. Last line is the src and dest
+
+###Output
+	 [ 3 1 ]
+  
+##Project Contributor
+
+* Dinesh Appavoo ([@DineshAppavoo](https://twitter.com/DineshAppavoo))
--- src/com/bfs/shortestpath/graph/BFSShortestPath.java
+++ src/com/bfs/shortestpath/graph/BFSShortestPath.java
@@ -17,44 +17,35 @@ import java.util.Stack;
  */
 public class BFSShortestPath {
 
-	
-	public static int noOfVertices,noOfEdges;
-	public static Graph graph=null;
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 
-	}
-	
-	public void constructGraph()
-	{
 
-		
-		int u, v, w;
+	public static Graph constructGraph()
+	{		
+		int noOfVertices,noOfEdges;
+		Graph graph=null;
+
+		int u, v;
 		Scanner scanner=new Scanner(System.in);
 		while(scanner.hasNext())
 		{
 			noOfVertices=scanner.nextInt();
 			noOfEdges=scanner.nextInt();
-					
-			
+
+
 			graph=new Graph(noOfVertices);
 			for(int i=0;i<noOfEdges;i++)
 			{
 				u=scanner.nextInt();
 				v=scanner.nextInt();
-				w=scanner.nextInt();
 				graph.addEdge(u, v);
 				graph.addEdge(v, u);
 			}
 			break;
 		}
 		//graph.printGraph();
+		return graph;
 	}
-	
+
 	/**
 	 * doBFSShortestPath(G, s, t):
 	 * 	L <- Empty
@@ -92,21 +83,21 @@ public class BFSShortestPath {
 	 * @param dest
 	 * @return
 	 */
-	
-	public ArrayList<Integer> doBFSShortestPath(Graph graph, int source, int dest)
+
+	public static ArrayList<Integer> doBFSShortestPath(Graph graph, int source, int dest)
 	{
 		ArrayList<Integer> shortestPathList = new ArrayList<Integer>();
 		HashMap<Integer, Boolean> visited = new HashMap<Integer, Boolean>();
-		
+
 		if (source == dest)
 			return null;
 		Queue<Integer> queue = new LinkedList<Integer>();
 		Stack<Integer> pathStack = new Stack<Integer>();
-		
+
 		queue.add(source);
 		pathStack.add(source);
 		visited.put(source, true);
-		
+
 		while(!queue.isEmpty())
 		{
 			int u = queue.poll();
@@ -124,8 +115,8 @@ public class BFSShortestPath {
 				}
 			}
 		}
-		
-		
+
+
 		//To find the path
 		int node, currentSrc=dest;
 		shortestPathList.add(dest);
@@ -140,8 +131,40 @@ public class BFSShortestPath {
 					break;
 			}
 		}
-		
+
 		return shortestPathList;
+	}
+
+
+	/**
+	 * 
+	 * Method to find the shortest path
+	 */
+	public static void findShortestPath()
+	{
+
+		Graph graph = constructGraph();
+		Scanner scanner = new Scanner(System.in);
+		int source = scanner.nextInt();
+		int dest = scanner.nextInt();
+
+		ArrayList<Integer> shortestPathList =  doBFSShortestPath(graph, source, dest);
+
+		System.out.print("[ ");
+		for(int node : shortestPathList)
+		{
+			System.out.print(node+" ");
+		}
+		System.out.print("]");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BFSShortestPath.findShortestPath();
 	}
 
 }
