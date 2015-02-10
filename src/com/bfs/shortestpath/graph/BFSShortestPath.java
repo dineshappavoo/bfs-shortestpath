7d6066d8996b15651dd2f851bc287fea999fe5d0
dinesgappavoo <dinesha.cit@gmail.com>
2015-02-10 01:19:40 -0600

    Update pseudocode

--- src/com/bfs/shortestpath/graph/BFSShortestPath.java
+++ src/com/bfs/shortestpath/graph/BFSShortestPath.java
@@ -55,6 +55,43 @@ public class BFSShortestPath {
 		//graph.printGraph();
 	}
 	
+	/**
+	 * doBFSShortestPath(G, s, t):
+	 * 	L <- Empty
+	 *  visited <- Empty
+	 *  Q <- Empty
+	 *  S <- Empty
+	 *  
+	 *  Q <- add(Q, s)
+	 *  S <- add(S, s)
+	 *  visited[s] <- true
+	 *  while(!Empty(Q))
+	 *  	u <- poll(Q)
+	 *  	for each vertex v in Adj[u]
+	 *  		if visited[v]!=true
+	 *  			Q <- add(Q,v)
+	 *  			S <- add(S,v)
+	 *  			visited[v] = true
+	 *  			if u == dest
+	 *  				break
+	 *  currentSrc=t
+	 *  L <- add(L,t)
+	 *  while(!Empty(S))
+	 *  	u <= poll(S)
+	 *  	if isNeighbor(G,currentSrc,u)
+	 *  		L <- add(L, u)
+	 *  		currentSrc = node;
+				if node == source
+					break;
+		return L
+	 *  		
+	 * 
+	 * 
+	 * @param graph
+	 * @param source
+	 * @param dest
+	 * @return
+	 */
 	
 	public ArrayList<Integer> doBFSShortestPath(Graph graph, int source, int dest)
 	{
